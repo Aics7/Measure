3f2323b0e577ac4a23a98d43cb4e4dc6bb083e53
Aics7 <issifu.alhassan@ashesi.edu.gh>
2019-10-20 20:52:29 +0000

    created buttons on main activity

--- app/src/main/java/com/example/measure/MainActivity.java
+++ app/src/main/java/com/example/measure/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.measure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void proximity(View view){
+        Intent prox=new Intent(MainActivity.this,Proximity.class);
+        startActivity(prox);
+    }
+    public void accelerometer(View view){
+        Intent accel=new Intent(MainActivity.this,Accelerometer.class);
+        startActivity(accel);
+    }
+
 }
