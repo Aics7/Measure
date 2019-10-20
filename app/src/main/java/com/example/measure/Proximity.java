dc5bf1b48f7da947a620b61d1535e2bd41ea7df2
Aics7 <issifu.alhassan@ashesi.edu.gh>
2019-10-20 23:10:57 +0000

    normal proximity reading

--- app/src/main/java/com/example/measure/Proximity.java
+++ app/src/main/java/com/example/measure/Proximity.java
@@ -1,13 +1,52 @@
 package com.example.measure;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Proximity extends AppCompatActivity {
+public class Proximity extends AppCompatActivity implements SensorEventListener {
+
+    TextView textView;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proximity);
+
+        textView = (TextView) findViewById(R.id.proximityTextView);
+        sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor= sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType()==sensor.TYPE_PROXIMITY){
+            textView.setText(""+ sensorEvent.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
